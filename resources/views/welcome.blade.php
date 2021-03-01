dd664800df39e99f2ae777789f8fcd35afdcd7dc
Henri <henri.phothinantha@etu.univ-smb.fr>
2021-03-01 09:41:06 +0100

    insertion nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,17 +68,7 @@
     <body>
         <div class="flex-center position-ref full height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" >Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" >Register</a>
-                        @endif
-                    @endauth
-                </div>
+                @extends('nav')
             @endif
             <div class="content">
                 <div class="title m-b-md">

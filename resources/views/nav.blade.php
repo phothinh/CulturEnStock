3a0ab4274474d05c458aa31fe5a4576cd7819ef9
Henri <henri.phothinantha@etu.univ-smb.fr>
2021-03-01 09:53:08 +0100

    insertion nav

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="{{ url('/home') }}">CulturEnStock</a>
+      <a class="navbar-brand" href="#">CulturEnStock</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="{{ url('/home') }}">Home</a></li>
+      <li class="active"><a href="#">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Page 1-1</a></li>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,10 @@
          @endif
 
          @section('content')
-         {{ __('CulturEnStock') }}
+         <div class="container">
+            <h3>Right Aligned Navbar</h3>
+            <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+        </div>
          @endsection
     </body>
 </html>
